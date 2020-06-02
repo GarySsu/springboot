fc7a246acf8935c303103109d2890c85fb65add3
GarySsu <og22563568@gmail.com>
2020-06-02 19:16:35 +0800

    add migration

---
+++ 01_spring_boot_flyway/src/main/resources/sql/migration/V1__spring_boot_security.sql
@@ -0,0 +1,19 @@
+#CREATE DATABASE IF NOT EXISTS spring_boot_example CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `createDate` date DEFAULT NULL,
+  `lastLoginTime` date DEFAULT NULL,
+  `enabled` int(5) DEFAULT NULL,
+  `accountNonExpired` int(5) DEFAULT NULL,
+  `accountNonLocked` int(5) DEFAULT NULL,
+  `credentialsNonExpired` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `authority` (
+  `id` int(10) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
