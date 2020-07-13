37b7627c315e75a935d9b0ca9fb8826fb9cdf27a
GarySsu <og22563568@gmail.com>
2020-07-13 18:45:48 +0800

    modify controller for spring_boot_telegram_bot

--- 06_spring_boot_telegram_bot/src/main/java/com/gary/controller/TelegramController.java
+++ 06_spring_boot_telegram_bot/src/main/java/com/gary/controller/TelegramController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,7 +39,7 @@ public class TelegramController  {
      * send message to channel by telegram bot
      * @return
      */
-    @GetMapping("/photo")
+    @PostMapping("/photo")
     public ResponseEntity sendPhoto(){
         try{
             telegramService.sendPhoto();
